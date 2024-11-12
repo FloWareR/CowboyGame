504a0fe7bd7b000834673b93679a6670757e92d0
Floware <rafaxtz3@gmail.com>
2024-11-11 19:07:50 -0600

    no message

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyLightPrefab;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
